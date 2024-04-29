d5c3f2a91c52fa791464eea99800bd567b480787
Liping Chen <lipingchen@Lipings-MacBook-Pro.local>
2024-04-29 11:10:02 -0400

    add address

--- components/landing/ContactUsSection.tsx
+++ components/landing/ContactUsSection.tsx
@@ -12,45 +12,90 @@ export default function ContactUsSection() {
                         <p className="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">
                             Maryland
                         </p>
-                        <p className="mb-3 text-gray-500 dark:text-gray-400">
-                            Linticum
-                            <br /> 957 Hill Hills Suite 491, United States
-                        </p>
-                        <ul className="space-y-3 text-gray-500 dark:text-gray-400">
-                            <li className="flex items-center">
-                                <svg
-                                    className="mr-2 h-5 w-5"
-                                    fill="currentColor"
-                                    viewBox="0 0 20 20"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        fillRule="evenodd"
-                                        d="M7 2a2 2 0 00-2 2v12a2 2 0 002 2h6a2 2 0 002-2V4a2 2 0 00-2-2H7zm3 14a1 1 0 100-2 1 1 0 000 2z"
-                                        clipRule="evenodd"
-                                    />
-                                </svg>
-                                Office: 443-577-8643
-                            </li>
-                            <li className="flex items-center">
-                                <svg
-                                    className="mr-2 h-5 w-5"
-                                    fill="currentColor"
-                                    viewBox="0 0 20 20"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                                    <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-                                </svg>
-                                Support:&nbsp;
-                                <a
-                                    href="mailto:company@name.com"
-                                    className="ml-2 hover:underline"
-                                >
-                                    Janelle@harmonyhaulage.com
-                                </a>
-                            </li>
-                        </ul>
+                        <div className="flex md:space-x-16 flex-col md:flex-row space-y-8 md:space-y-0">
+                            <div className="flex-1">
+                                <p className="mb-3 text-gray-500 dark:text-gray-400">
+                                    Physical Address
+                                    <br />881 Concorde circle  , Linthicum heights md 21090
+                                </p>
+                                <ul className="space-y-3 text-gray-500 dark:text-gray-400">
+                                    <li className="flex items-center">
+                                        <svg
+                                            className="mr-2 h-5 w-5"
+                                            fill="currentColor"
+                                            viewBox="0 0 20 20"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path
+                                                fillRule="evenodd"
+                                                d="M7 2a2 2 0 00-2 2v12a2 2 0 002 2h6a2 2 0 002-2V4a2 2 0 00-2-2H7zm3 14a1 1 0 100-2 1 1 0 000 2z"
+                                                clipRule="evenodd"
+                                            />
+                                        </svg>
+                                        Office: 443-577-8643
+                                    </li>
+                                    <li className="flex items-center">
+                                        <svg
+                                            className="mr-2 h-5 w-5"
+                                            fill="currentColor"
+                                            viewBox="0 0 20 20"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+                                            <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+                                        </svg>
+                                        Email:&nbsp;
+                                        <a
+                                            href="mailto:company@name.com"
+                                            className="ml-2 hover:underline"
+                                        >
+                                            Janelle@harmonyhaulage.com
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="flex-1">
+                                <p className="mb-3 text-gray-500 dark:text-gray-400">
+                                    Mailing Address
+                                    <br />881 Concorde circle  , Linthicum heights md 21090
+                                </p>
+                                <ul className="space-y-3 text-gray-500 dark:text-gray-400">
+                                    <li className="flex items-center">
+                                        <svg
+                                            className="mr-2 h-5 w-5"
+                                            fill="currentColor"
+                                            viewBox="0 0 20 20"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path
+                                                fillRule="evenodd"
+                                                d="M7 2a2 2 0 00-2 2v12a2 2 0 002 2h6a2 2 0 002-2V4a2 2 0 00-2-2H7zm3 14a1 1 0 100-2 1 1 0 000 2z"
+                                                clipRule="evenodd"
+                                            />
+                                        </svg>
+                                        Office: 443-577-8643
+                                    </li>
+                                    <li className="flex items-center">
+                                        <svg
+                                            className="mr-2 h-5 w-5"
+                                            fill="currentColor"
+                                            viewBox="0 0 20 20"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+                                            <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+                                        </svg>
+                                        Email:&nbsp;
+                                        <a
+                                            href="mailto:company@name.com"
+                                            className="ml-2 hover:underline"
+                                        >
+                                            Janelle@harmonyhaulage.com
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
                     </address>
                 </div>
                 <Footer.Divider />
