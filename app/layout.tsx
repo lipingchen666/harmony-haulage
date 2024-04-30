41806ed10be5866e8ecab2bbcb0767be2a59c3b1
Liping Chen <lipingchen@Lipings-MacBook-Pro.local>
2024-04-29 22:11:50 -0400

    add image preload

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="preload" as="image" href="/truck.jpg" />
         <ThemeModeScript />
       </head>
       <body className={inter.className}>{children}</body>
